176577606c716bb7c6ede8fc012283a716143655
Irina Mateush <ismateush@yandex-team.ru>
2020-06-11 21:12:55 +0300

    начинаем программировать

---
+++ src/components/app/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+
+
+const App = () => {
+  return (
+    <WelcomeScreen
+      errors = {3}
+    />
+  );
+};
+
+
+export default App;
